76e840edef8c4ee34f4c15504f60fd8029d5f760
Christopher Norman <christophernorman@Christophers-MacBook-Pro-2.local>
2019-07-21 15:48:11 -0700

    Base setup with navigation

---
+++ components/nav.php
@@ -0,0 +1,11 @@
+<div class="ui menu">
+  <a href="./" class="header item">
+    Secure Now
+  </a>
+  <a href="./guard.php" class="item">
+    Guard
+  </a>
+  <a href="./client.php" class="item">
+    Client
+  </a>
+</div>
---
+++ index.php
@@ -0,0 +1,14 @@
+<html class="no-js" lang="en">
+  <head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Secure Now</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css">
+  </head>
+  <body>
+	<?php include 'components/nav.php'; ?>
+
+	<h1>This is the index</h1>
+	<script src="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.js"></script>
+  </body>
+</html>
